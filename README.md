25ac20520ef2822933b68976583dc20fa720b128
Sunny Kumar Singh <SRajukumarsingh@ivycomptech.com>
2024-10-21 20:03:06 +0530

    Setup ROuting using router components and Outlet

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 - Create a vite application using npm create vite@latest devTinder-web -- --template react
 - Install tailwind and inject the dependancies
 - Install Daisy UI as npm i -D daisyui@latest
-- It is compatible 
+- Use Router components as routing in this project, earlier we ad used createBrowserRouter
+- Keep Navbar and footer as intact and keep others in <Outlet/>
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import Navbar from "./Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import Body from "./Body";
 
 function App() {
-
   return (
     <>
-      <Navbar />
-      <h1>Dev Tinder</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
